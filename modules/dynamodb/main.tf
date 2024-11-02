b800ed42dea567ab1e32498194a67df329a483cc
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-11-02 15:23:11 +0530

    fix : error in range key

--- modules/dynamodb/main.tf
+++ modules/dynamodb/main.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "users" {
-  name         = "UsersTable"
+  name         = var.table_name
   billing_mode = "PAY_PER_REQUEST"
 
   hash_key = "userId"
@@ -9,13 +9,53 @@ resource "aws_dynamodb_table" "users" {
   }
 
   global_secondary_index {
-    name            = "user_email_index" # Name for your GSI
-    hash_key        = "email"            # Attribute for GSI hash key
-    range_key       = "userName"         # Optional range key for GSI (can be omitted)
+    name     = "user_email_index" # Name for your GSI
+    hash_key = "email"            # Attribute for GSI hash key
+    #range_key       = "userName"         # Optional range key for GSI (can be omitted)
     projection_type = "ALL"
-
   }
 
   stream_enabled   = true
-  stream_view_type = "NEW_AND_OLD_IMAGES" # Change to "NEW_IMAGE" or "OLD_IMAGE" for different stream view types
+  stream_view_type = "NEW_AND_OLD_IMAGES"
+
+  server_side_encryption {
+    enabled = true
+  }
+
+  tags = {
+    Name = "UsersTable"
+  }
+}
+
+# Item 1
+resource "aws_dynamodb_table_item" "user_item_1" {
+  table_name = aws_dynamodb_table.users.name
+  hash_key   = "userId"
+  item = jsonencode({
+    "userId" : "user123",
+    "userName" : "Luke Skywalker",
+    "email" : "usetheforceluke@example.com"
+  })
+}
+
+# Item 2
+resource "aws_dynamodb_table_item" "user_item_2" {
+  table_name = aws_dynamodb_table.users.name
+  hash_key   = "userId"
+  item = jsonencode({
+    "userId" : "user456",
+    "userName" : "Leia Organa",
+    "email" : "princessleia@example.com"
+  })
+}
+
+# Item 3
+resource "aws_dynamodb_table_item" "user_item_3" {
+  table_name = aws_dynamodb_table.users.name
+  hash_key   = "userId"
+  item = jsonencode({
+    "userId" : "user789",
+    "userName" : "Han Solo",
+    "email" : "hansolo@example.com"
+  })
 }
