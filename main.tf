d68dc5f07dc087906d60ddc74bb572abe542ceae
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-11-04 13:45:18 +0530

    test1

--- main.tf
+++ main.tf
@@ -2,13 +2,17 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-module "test_iam_role" {
-  source    = "./modules/Iam_role"
-  role_name = "lambdaFunctionRole"
-}
+# module "test_iam_role" {
+#   source    = "./modules/Iam_role"
+#   role_name = "lambdaFunctionRole"
+# }
 
 
 # module "dynamodb" {
 #   source     = "./modules/dynamodb"
 #   table_name = "UsersTable"
 # }
+
+module "sns" {
+  source = "./modules/sns"
+}
