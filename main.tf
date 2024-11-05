9f2964f439efea9e968a1643909c88b5dde5de51
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-11-05 11:00:07 +0530

    feat: complete event-driven architecture with Database Trigger using DynamoDB, Streams, SNS, and Lambda

--- main.tf
+++ main.tf
@@ -2,23 +2,23 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-module "Iam_role" {
-  source    = "./modules/Iam_role"
-  role_name = "lambdaFunctionRole"
-}
+# module "Iam_role" {
+#   source    = "./modules/Iam_role"
+#   role_name = "lambdaFunctionRole"
+# }
 
 
-module "dynamodb" {
-  source     = "./modules/dynamodb"
-  table_name = "UsersTable"
-}
+# module "dynamodb" {
+#   source     = "./modules/dynamodb"
+#   table_name = "UsersTable"
+# }
 
-module "sns" {
-  source = "./modules/sns"
-}
+# module "sns" {
+#   source = "./modules/sns"
+# }
 
-module "lambda_function" {
-  source     = "./modules/lambda"
-  role_arn   = module.Iam_role.function_role_arn
-  source_arn = module.dynamodb.event_source_arn
-}
+# module "lambda_function" {
+#   source     = "./modules/lambda"
+#   role_arn   = module.Iam_role.function_role_arn
+#   source_arn = module.dynamodb.event_source_arn
+# }
