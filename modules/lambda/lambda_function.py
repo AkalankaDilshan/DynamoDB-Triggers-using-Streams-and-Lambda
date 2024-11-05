f7d6779162d648f4c424e16aae75ecb094a2493f
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-11-05 09:44:12 +0530

    fix lambda function

--- modules/lambda/lambda_function.py
+++ modules/lambda/lambda_function.py
@@ -1,4 +1,4 @@
-import json
+# import json
 import boto3
 
 def lambda_handler(event, context):
@@ -6,34 +6,21 @@ def lambda_handler(event, context):
     account_id = context.invoked_function_arn.split(":")[4]
     region = context.invoked_function_arn.split(":")[3]
     
-    # Set the SNS topic name (replace 'TableChangeAlerts' with your actual SNS topic name)
-    sns_topic_name = 'TableChangeAlerts'
+    
+    sns_topic_name = 'User_table_sns'
 
     for record in event['Records']:
-        # Process the event type: INSERT, MODIFY, REMOVE
-        event_name = record['eventName']
-        if event_name == 'INSERT':
-            # Handle new item inserted
-            new_image = record['dynamodb']['NewImage']
-            message = 'New item added'
-        elif event_name == 'MODIFY':
-            # Handle item modified
-            old_image = record['dynamodb']['OldImage']
-            new_image = record['dynamodb']['NewImage']
-            message = 'Item modified'
-        elif event_name == 'REMOVE':
-            # Handle item removed
-            old_image = record['dynamodb']['OldImage']
-            message = "Item removed"
+        new_image = record['dynamodb']['NewImage']
+        user_id   = new_image['userId']['S']
         
         # Publish the message to SNS
         response = sns.publish(
             TopicArn=f'arn:aws:sns:{region}:{account_id}:{sns_topic_name}',
-            Message=message,
+            Message=f'{user_id} has been changed',
             Subject='Database Changes'
         )
     
-    return {
-        "statusCode": 200,
-        "body": json.dumps({"message": message, "sns_response": response})
-    }
+    # return {
+    #     "statusCode": 200,
+    #     "body": json.dumps({"message": message, "sns_response": response})
+    # }
