7c7d397c7a99b390e1b1177f721641be98f417f1
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-11-10 11:20:53 +0530

    feat(workflow) : change workflow

--- terraform.tf
+++ terraform.tf
@@ -8,3 +8,14 @@ terraform {
     }
   }
 }
+
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 4.16"
+#     }
+#   }
+
+#   required_version = ">= 1.2.0"
+# }
